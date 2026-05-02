1625e981e773c406ece8e7d2821b68ed1b9c0836
Blake Loring <blake@parsed.uk>
2026-05-02 19:58:17 +0800

    .

--- lib/axi4_cache.mli
+++ lib/axi4_cache.mli
@@ -11,7 +11,7 @@ module type Config = sig
 end
 
 module Make (Config : Config) (Memory_bus : Memory_bus_intf.S) (Axi4_out : Axi4.S) : sig
-        module Ram : Cache_ram_intf.S
+  module Ram : Cache_ram_intf.S
 
   module Memory_requests : sig
     type 'a t =
